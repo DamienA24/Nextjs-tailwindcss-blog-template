21b3ddb26b8ac3ab195aca53c61fc3d3992906f1
DamienA24 <alves.d24@laposte.net>
2024-03-25 13:17:48 +0100

    refactor(utils):change site url

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -7,7 +7,7 @@ const siteMetadata = {
     "Un blog dédié à l'exploration des pédagogies innovantes pour l'éducation des enfants, avec un accent sur Montessori et d'autres approches éducatives.",
   language: "fr-fr",
   theme: "system", // system, dark or light
-  siteUrl: "https://create-blog-with-nextjs.vercel.app", // your website URL
+  siteUrl: "https://eveilcurieux.com", // your website URL
   siteLogo: "/cactus-bg.png",
   socialBanner: "/social-banner.png", // add social banner in the public folder
   locale: "fr-FR",
