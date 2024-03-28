c0332639aecf5e74deacf4cf1c9a51b8e0483f46
DamienA24 <alves.d24@laposte.net>
2024-03-28 08:22:06 +0100

    fix(utils): add www on canonic url

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -7,7 +7,7 @@ const siteMetadata = {
     "Un blog dédié à l'exploration des pédagogies innovantes pour l'éducation des enfants, avec un accent sur Montessori et d'autres approches éducatives.",
   language: "fr-fr",
   theme: "system", // system, dark or light
-  siteUrl: "https://eveilcurieux.com", // your website URL
+  siteUrl: "https://www.eveilcurieux.com", // your website URL
   siteLogo: "/cactus-bg.png",
   socialBanner: "/social-banner.png", // add social banner in the public folder
   locale: "fr-FR",
