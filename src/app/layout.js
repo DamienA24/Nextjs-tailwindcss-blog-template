271eddf2f3b067e8bc165d3a2d2029735f030e6d
DamienA24 <alves.d24@laposte.net>
2024-03-29 09:45:12 +0100

    feat(layout): add google tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -65,12 +65,25 @@ export default function RootLayout({ children }) {
           "font-mr bg-light dark:bg-dark"
         )}
       >
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-54KXSP4Q"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
         <Script id="theme-switcher" strategy="beforeInteractive">
           {`if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
   } else {
     document.documentElement.classList.remove('dark')
   }`}
+        </Script>
+        <Script>
+          {` (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-54KXSP4Q');`}
         </Script>
         <Header />
         {children}
