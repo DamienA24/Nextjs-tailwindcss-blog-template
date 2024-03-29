9b43d9f95491fb581bfa7a70e2d5654eb1d0e923
DamienA24 <alves.d24@laposte.net>
2024-03-29 09:25:42 +0100

    refactor(layout): change lang en to fr

--- src/app/layout.js
+++ src/app/layout.js
@@ -57,7 +57,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body
         className={cx(
           inter.variable,
